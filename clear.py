321f309ed958934552010d60c6e3dc4c2d8e2354
xabertum <mohatar@yahoo.com>
2017-07-07 18:17:27 +0200

    minor changes..

---
+++ clear.py
@@ -0,0 +1,4 @@
+from sense_hat import *
+
+sense = SenseHat();
+sense.clear()
--- temp_txt.py
+++ temp_txt.py
@@ -7,10 +7,18 @@ sense.set_rotation(180)
 red = [255, 0, 0]
 green = [0,255,0]
 blue = [0,0,255]
+color = str(raw_input('indique el color de la temperatura'))
 
 
-temp = round(sense.get_temperature(),2)
-temp_str = 'Temp: ' + str(temp)
+def probando():
+    temp = round(sense.get_temperature(),2)
+    temp_str = 'Temp: ' + str(temp)
 
-sense.show_message(temp_str,0.1, green)
+    sense.show_message(temp_str,0.1,color,blue)
 
+    pressure = sense.get_pressure()
+    press_str = 'Press: ' + str(pressure)
+
+    sense.show_message(press_str,0.1,blue)
+
+probando()
